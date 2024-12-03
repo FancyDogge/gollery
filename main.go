a1d6c6ee083cdc9301dfe7794489a8a4e900a826
Daniil Nosov <fancy.dogge@gmail.com>
2024-12-03 23:48:17 +0300

    lul

--- main.go
+++ main.go
@@ -19,18 +19,42 @@ http.ResponseWriter
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//вообще го смотрит на байты в респонс теле и определяет тип контент тайпа(опять же, вроде бы)
 	//но вот так уот можно установить данный хедер мануально
-	w.Header().Set("Content-Type", "text/plainж charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, "<h1>ZULUL</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "plain/text; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
+// Я дурак и пытался написать уже написанное
+// func notFoundError(w http.ResponseWriter, r *http.Request) {
+// 	http.Error(w, "404 page not found", 404)
+// }
+
+// router курильщика, чтобы разобраться в происходящем
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		У нашего http.Request есть поле (field) - URL *url.URL
+		Это поинтер на URL struct, у которого в свою очередь есть поле Path string // path (relative paths may omit leading slash)
+		Поэтому мы достаем все это дело через точки .
+	*/
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		//return 404 если пути не существует
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	//добавляем роутер курильщика в рут путь / чтобы там он уже отдавал нужный хэндлер
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on port :3000...")
 	http.ListenAndServe(":3000", nil)
 }
